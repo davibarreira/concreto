25a570a81863aaf3ebe145071e01e007316de39a
Davi Barreira <davibar1@gmail.com>
2014-06-18 09:34:24 -0300

    Dimensionar viga, adicionado

--- VIGA.py
+++ VIGA.py
@@ -31,9 +31,10 @@ class Viga():
                  Para concreto utiliza-se 1.4 e para o aco 1.15
             
         '''
-        self.h      = h*100
-        self.bw     = bw*100
-        self.fyk    = CA*10**7
+        self.h      = h*100.0
+        self.d      = (h-6.0)*100.0
+        self.bw     = bw*100.0
+        self.fyk    = CA*10.0**7
         self.fyd    = self.fyk/1.15
         self.fcd    = fck/1.4
         self.Ecs    = 0.85*5600*((fck*10**(-6))**0.5)*10**6
@@ -41,57 +42,61 @@ class Viga():
         self.Asmin=0.0015*self.h*self.bw
         self.Asmax=0.04*self.h*self.bw
         
-        def Dimensionar(Mk,Md=False):
-            '''
-            DIMENSIONAR: Essa funcao faz o dimensionamento da armadura da viga simples.
+    def Dimensionar(self,Mk,Md=False):
+        '''
+        DIMENSIONAR: Essa funcao faz o dimensionamento da armadura da viga simples.
+        
+        INPUT:
+            Mk         = Momento Caracteristico maximo atuando na viga
+            Md         = Momento de Projeto maximo atuando na viga
+        
+        STORED:
+            As         = area de aco na viga (cm2)
+            Asl        = area de aco superior (cm2)
+            dominio    = dominio de deformacao
+            Md         = momento de projeto
+        '''
+        
+        self.Md       = (Mk*1.4)*10**3
+        dl       = 0.05
+        self.Asl      = 0.0 #Valor de area de armadura superior. O default eh 0.
+        #Resolvendo para 'x' - eq de 2o grau
+        a = -0.272*self.bw*self.fcd
+        b = 0.68*self.d*self.bw*self.fcd
+        c = -self.Md
+        x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+        x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
+        dominio=1
+        if x2>self.d or x2<0: x = x1
+        
+        #Determinando o dominio
+        
+        if x<=0:dominio=1
+        elif x>0 and x<=0.259*self.d: dominio=2
+        elif x>0.259*self.d and x<=(0.0035*self.d)/(self.eyd+0.0035): dominio=3
+        elif x>(0.0035*self.d)/(self.eyd+0.0035) and x<=self.h: dominio=4
+        else: dominio=5
+        
+        
+        #Estimando a area de armadura
+        if dominio==4:
+            x34 = 0.0035*self.d/(self.eyd+0.0035)
+            M34 = (0.85*self.fcd*self.bw*0.8*x34)*(self.d-0.4*x34)
+            As34= M34/(self.fyd*(self.d-0.4*x34))
+            M2  = self.Md-M34
+            esl = 0.35*(x34-dl)/x34 #epsilon` , a deformacao da armadura superior para retirar do dominio 4
+            if esl<self.eyd: fsl = self.Ey*esl/1.15
+            else: fsl = self.fyd
+            self.Asl = M2/((self.d-dl)*fsl)
+            self.As  = self.Asl + As34
             
-            INPUT:
-                Mk         = Momento Caracteristico maximo atuando na viga
-                Md         = Momento de Projeto maximo atuando na viga
+        else:
+            self.As = self.Md/(self.fyd*(self.d-0.4*x))
+        
             
-            STORED:
-                As         = area de aco na viga (cm2)
-                Asl        = area de aco superior (cm2)
-                dominio    = dominio de deformacao
-                Md         = momento de projeto
-            '''
-            
-            self.Md  = Mk*1.4
-            dl       = 0.05
-            #Resolvendo para 'x' - eq de 2o grau
-            a = -0.272*self.bw*self.fcd
-            b = 0.68*self.d*self.bw*self.fcd
-            c = -self.Md
-            x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
-            x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
-            if x2>d or x2<0: x = x1
-            
-            #Determinando o dominio
-            
-            if x<=0:dominio=1
-            elif x>0 and x<=0.259*d: dominio=2
-            elif x>0.259*self.d and x<=(0.0035*d)/(self.eyd+0.0035): dominio=3
-            elif x>(0.0035*d)/(self.eyd+0.0035) and x<=h: dominio=4
-            else: dominio=5
-            
-            
-            #Estimando a area de armadura
-            if dominio==4:
-                x34 = 0.0035*self.d/(self.eyd+0.0035)
-                M34 = (0.85*self.fcd*self.bw*0.8*x34)*(self.d-0.4*x34)
-                As34= M34/(self.fyd*(self.d-0.4*x34))
-                M2  = self.Md-M34
-                esl = 0.35*(x34-dl)/x34 #epsilon` , a deformacao da armadura superior para retirar do dominio 4
-                if esl<self.eyd: fsl = self.Ey*esl/1.15
-                else: fsl = self.fyd
-                self.Asl = M2/((self.d-dl)*fsl)
-                self.As  = self.Asl + As34
-                
-            else:
-                self.As = self.Md/(self.fyd*(self.d-0.4*x))
-            
-                
-            self.As = self.As*10**4 
-            
-            
+        self.As     = self.As*10**4
+        self.Asl    = self.Asl*10**4
+
+        
 a = Viga(100, 50, 25, 50)
+a.Dimensionar(10.0)
