81e1b600d7c4e86ef7bbd9aec29c831c144a7a98
Davi Sales Barreira <davi@new-host.com>
2015-03-11 09:42:59 -0400

    Adicionado o Info da norma. Ajeitei alinhamento de comentários

--- draft_files/vigafissura.py
+++ draft_files/vigafissura.py
@@ -3,19 +3,19 @@ import numpy as np
 '''Dimensionamento da armadura para viga de secao retangular'''
 
 '''INPUT'''
-bw = 25              #Em cm
-h  = 80              #Em cm
-fck= 25              #Em MPa
-CA = 50              #
-Md = 300            #Em kN.m
-Ey = 210*10**3      #Em MPa
-d  = 75.5           #Em cm
-ae = Ey/(0.85*5600*(fck)**0.5)#8.82          #Relacao entre os modulos de elasticidade (Es/Ec)
+bw = 25                                    # Em cm
+h  = 80                                    # Em cm
+fck= 25                                    # Em MPa
+CA = 50                                    #
+Md = 300                                   # Em kN.m
+Ey = 210*10**3                             # Em MPa
+d  = 75.5                                  # Em cm
+ae = Ey/(0.85*5600*(fck)**0.5)             # 8.82                   # Relacao entre os modulos de elasticidade (Es/Ec)
 
-bitola  = 20        #mm bitola em analise
-As      = 15.75         #Em cm2
-ni      = 2.25      #Depende do tipo de aco
-pcri    = 0.032      #pcri = Asi/Acri
+bitola  = 20                               # mm bitola em analise
+As      = 15.75                            # Em cm2
+ni      = 2.25                             # Depende do tipo de aco
+pcri    = 0.032                            # pcri = Asi/Acri
 
 fctm    = 0.3*fck**(2./3.)
 ''' INPUT - End '''
@@ -26,7 +26,7 @@ I2  = bw*Xln**3/3.0 + ae*As*(d-Xln)**2
 print'I2 = ',I2,'cm4'
 print 'xln=', Xln
 
-Fs  = ae*Md*1000*(d-Xln)/I2             #Em kN/mPa
+Fs  = ae*Md*1000*(d-Xln)/I2                # Em kN/mPa
 
 print 'Fs = ',Fs,'kN/cm2'
 
