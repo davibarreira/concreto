bb5c36f3a4e4c8a60b80483d8c5c0930d07d33f5
Davi Sales Barreira <davibar1@gmail.com>
2015-03-26 22:29:26 -0400

    Implementado o método de DimensionarT. Falta testar

--- Viga.java
+++ Viga.java
@@ -3,6 +3,8 @@ import static java.lang.Math.pow;
 public class Viga {
 
     public double h, d,bw,fck,fyk,fyd,fcd,eyd,Asmin,Asmax, Ecs,Ey;
+    public double hf   = 0.0;
+    public double bf   = 0.0;
     public double As   = 0.0;
     public double Asl  = 0.0;
     public int dominio = 1;
@@ -81,7 +83,12 @@ public class Viga {
                    this.dominio=3;
                 }
                 else{
-                    this.dominio=5;
+                    if (x>(0.0035*this.d)/(this.eyd+0.0035) && x<=this.h) {
+                       this.dominio = 4; 
+                    }
+                    else{
+                        dominio = 5;
+                    }
                 }
             }
         }
@@ -113,5 +120,143 @@ public class Viga {
         this.Asl    = this.Asl*pow(10.0,4.0);
     }
 
+    public void DimensionarT(double Mk, double bf, double hf){
+        double a,b,c,x1,x2,x,x34,M34,As34,M2,esl,dl,fsl,Mf,Af,Ma,Aa;
+
+        this.hf = hf/100.0;
+        this.bf = bf/100.0;
+        
+        this.Md = (Mk*1.4)*1000.0;
+        dl      = 0.05;
+
+        a = -0.272*this.bf*this.fcd;
+        b =  0.68*this.d*this.bf*this.fcd;
+        c = -this.Md;
+        x1 = (-b+pow(b*b-4*a*c,0.5))/(2*a);
+        x2 = (-b-pow(b*b-4*a*c,0.5))/(2*a);
+        if (x2>this.d || x2<0) {
+            x = x1;
+        }
+        else{
+            x = x2;
+        }
+
+        if (x*0.8>hf) {
+
+            Mf = 0.85*this.fcd*hf*(bf-this.bw)*(this.d-hf/2.0); 
+            Af = Mf/((this.d-hf/2.0)*this.fyd);  
+            Ma = this.Md-Mf;
+            a = -0.272*this.bw*this.fcd;
+            b =  0.68*this.d*this.bw*this.fcd;
+            c = -Ma;
+            x1 = (-b+pow(b*b-4*a*c,0.5))/(2*a);
+            x2 = (-b-pow(b*b-4*a*c,0.5))/(2*a);
+            if (x2>this.d || x2<0) {
+                x = x1;
+            }
+            else{
+                x = x2;
+            }
+
+            if (x<=0) {
+                this.dominio=1;
+            }
+            else{
+                if (x>0 && x<=0.259*this.d) {
+                   this.dominio=2; 
+                }
+                else{
+                    if (x>0.259*this.d && x<=(0.0035*this.d)/(this.eyd+0.0035)) {
+                       this.dominio=3;
+                    }
+                    else{
+                        if (x>(0.0035*this.d)/(this.eyd+0.0035) && x<=this.h) {
+                           this.dominio = 4; 
+                        }
+                        else{
+                            dominio = 5;
+                        }
+                    }
+                }
+            }
+
+            if (this.dominio==4) {
+                x34 = 0.0035*this.d/(this.eyd+0.0035);
+                M34 = (0.85*this.fcd*this.bw*0.8*x34)*(this.d-0.4*x34);
+                As34= M34/(this.fyd*(this.d-0.4*x34));
+                M2  = this.Md-M34;
+                esl = 0.35*(x34-dl)/x34;
+
+                if (esl<this.eyd) {
+                    fsl = this.Ey*esl/1.15;
+                }
+                else{
+                    fsl = this.fyd;
+                }
+
+                this.Asl = M2/((this.d-dl)*fsl);
+                Aa  = this.Asl + As34;
+
+            }
+
+            else{
+                Aa = Ma/(this.fyd*(this.d-0.4*x));
+            }
+
+            this.As = Aa + Af;
+        }
+
+        else{
+            if (x<=0) {
+                this.dominio=1;
+            }
+            else{
+                if (x>0 && x<=0.259*this.d) {
+                   this.dominio=2; 
+                }
+                else{
+                    if (x>0.259*this.d && x<=(0.0035*this.d)/(this.eyd+0.0035)) {
+                       this.dominio=3;
+                    }
+                    else{
+                        if (x>(0.0035*this.d)/(this.eyd+0.0035) && x<=this.h) {
+                           this.dominio = 4; 
+                        }
+                        else{
+                            dominio = 5;
+                        }
+                    }
+                }
+            }
+
+            if (this.dominio==4) {
+                x34 = 0.0035*this.d/(this.eyd+0.0035);
+                M34 = (0.85*this.fcd*this.bw*0.8*x34)*(this.d-0.4*x34);
+                As34= M34/(this.fyd*(this.d-0.4*x34));
+                M2  = this.Md-M34;
+                esl = 0.35*(x34-dl)/x34;
+
+                if (esl<this.eyd) {
+                    fsl = this.Ey*esl/1.15;
+                }
+                else{
+                    fsl = this.fyd;
+                }
+
+                this.Asl = M2/((this.d-dl)*fsl);
+                this.As  = this.Asl + As34;
+
+            }
+
+            else{
+                this.As = this.Md/(this.fyd*(this.d-0.4*x));
+            }
+
+        this.As     = this.As*pow(10.0,4.0);
+        this.Asl    = this.Asl*pow(10.0,4.0);
+
+        }
+        
+    }
 
 }
